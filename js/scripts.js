ad87ec763a6d26740898d4ff908f2a679bc39231
John <johnpaulo.beyong0816@gmail.com>
2020-04-10 01:29:01 +0800

    Improved code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,24 @@
 let width, height;
-let canvas, c;
+let canvas, ctx;
 
 function createCanvas(w, h) {
-  if (canvas !== undefined) {
-    canvas.parentElement.removeChild(canvas);
-  }
-  canvas = document.createElement('canvas');
-  document.body.appendChild(canvas);
-  c = canvas.getContext('2d');
-  canvas.width = w;
-  canvas.height = h;
-  width = w;
-  height = h;
+	if (canvas) canvas.parentElement.removeChild(canvas);
+	canvas = document.createElement('canvas');
+	document.body.appendChild(canvas);
+	ctx = canvas.getContext('2d');
+	canvas.width = w;
+	canvas.height = h;
+	width = w;
+	height = h;
 }
 
 function init() {
-  createCanvas(320, 320);
-  render();
+	createCanvas(320, 320);
+	render();
 }
 
 function render() {
-  requestAnimationFrame(render);
-  c.fillStyle = 'gray';
-  c.fillRect(0, 0, width, height);
+	requestAnimationFrame(render);
+	ctx.fillStyle = 'gray';
+	ctx.fillRect(0, 0, width, height);
 }
-
-init();
